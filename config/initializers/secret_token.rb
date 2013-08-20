d5bd75e76ca5730c3afea21e8238997e39e8a306
mbp <mbp@mbpmatoMacBook-Pro.local>
2013-08-20 15:44:13 +0800

    create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eshop::Application.config.secret_key_base = 'b71538b26bbaed937f470475c6ff3c27bb07553f2b56c0ee1bfacc8c3380c3bcebf0fe883a86aed8b359d433031830bd28564e003aa7db7bcb7a2315dd4bc1fe'
