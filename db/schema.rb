8903dc6a76dc6060cae0fcfcf89bb5a53ede67fc
zhzenghui <zhzenghui@gmail.com>
2013-08-26 23:43:55 +0800

    reset user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130822074253) do
+ActiveRecord::Schema.define(version: 20130826154136) do
+
+  create_table "_likes_old_20130822", force: true do |t|
+    t.string   "ll_id"
+    t.string   "user_id"
+    t.string   "nick"
+    t.integer  "type"
+    t.integer  "status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "favorites", force: true do |t|
     t.integer  "item_numid"
